ae85f0c29734df7e37ad4d8e1ad5fd843d0bf8b0
Volodymyr Yahello <vyahello@gmail.com>
2020-10-13 23:33:11 +0300

    Parametrize unittests for app routes

--- setup.py
+++ setup.py
@@ -4,34 +4,38 @@ from setuptools import setup, find_packages
 
 def _description() -> str:
     """Returns project description."""
-    with open("README.md", "r") as readme:  # type: IO[str]
+    with open("README.md") as readme:  # type: IO[str]
         return readme.read()
 
 
 def _requirements() -> Sequence[str]:
     """Returns requirements sequence."""
-    with open("requirements.txt", "r") as requirements:  # type: IO[str]
+    with open("requirements.txt") as requirements:  # type: IO[str]
         return tuple(map(str.strip, requirements.readlines()))
 
 
-setup(
-    name="calorie-counter",
-    version="0.1.3",
-    author="Volodymyr Yahello",
-    author_email="vyahello@gmail.com",
-    description="This project represents simple web app to calculate calories based on given food type.",
-    long_description=_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/vyahello/calorie-counter",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=_requirements(),
-    classifiers=(
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
-    python_requires=">=3.6",
-)
+if __name__ == "__main__":
+    setup(
+        name="calorie-counter",
+        version="0.1.3",
+        author="Volodymyr Yahello",
+        author_email="vyahello@gmail.com",
+        description=(
+            "This project represents simple web app to "
+            "calculate calories based on given food type."
+        ),
+        long_description=_description(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/vyahello/calorie-counter",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=_requirements(),
+        classifiers=(
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        python_requires=">=3.6",
+    )
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,12 +3,12 @@ from counter import application
 from flask.testing import Client
 
 
-def __is_code(app: Client, rule: str, code: str) -> bool:
-    return code in app.get(rule)[1]
+def __is_code(app: Client, route: str, code: str) -> bool:
+    return code in app.get(route)[1]
 
 
-def __is_success(app: Client, rule: str) -> bool:
-    return __is_code(app, rule, code='200')
+def __is_success(app: Client, route: str) -> bool:
+    return __is_code(app, route, code='200')
 
 
 @pytest.fixture(scope='module')
@@ -16,13 +16,6 @@ def app() -> Client:
     return Client(application)
 
 
-def test_root(app: Client) -> None:
-    assert __is_success(app, rule='/')
-
-
-def test_index(app: Client) -> None:
-    assert __is_success(app, rule='/index')
-
-
-def test_home(app: Client) -> None:
-    assert __is_success(app, rule='/home')
+@pytest.mark.parametrize('route', ('/', '/index', '/home'))
+def test_route(app: Client, route: str) -> None:
+    assert __is_success(app, route)
